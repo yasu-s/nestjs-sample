ed87bbd8199045696d86db03dd91741bdf57a31e
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-05-12 17:18:04 +0900

    GraphQLModule追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({ typePaths: ['./**/*.graphql'] })],
   controllers: [AppController],
   providers: [AppService],
 })
